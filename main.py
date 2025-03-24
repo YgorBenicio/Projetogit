5bed50fd22bb52731ef7edfc8a69e58db530e0a1
YgorBenicio <ygorbenicio1704@gmail.com>
2025-03-24 15:12:49 -0300

    segundo commit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("gay")
+quantidade_alunos = int(input("Digite a quantidade de alunos:"))
+  
+
+for numero_aluno in range(1, quantidade_alunos + 1):
+        print("Digite a nota do aluno", numero_aluno)
+        nota = float(input())
+        if nota >= 7:
+            print("Aprovado")
+        elif nota >= 5:
+            print("Recuperação")
+        else:
+            print("Reprovado")
